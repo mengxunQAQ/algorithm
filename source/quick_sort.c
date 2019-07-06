6d3d92b54a3e4e6ddfab91bc5809eb4a5f89b320
mengxun <mengxun1122@163.com>
2019-07-06 14:46:37 +0800

    add quick_sort

--- source/quick_sort.c
+++ source/quick_sort.c
@@ -1,19 +1,45 @@
-# include <stdio.h>
+/* 快速排序 */
+#include<stdio.h> 
 
-void quick_sort(int a[]);
+void quicksort(int [], int, int);
 
-int main() {
-	int arr[] = {21, 16, 7, 55, 23};
-	quick_sort(arr);
-	for (i = 0; i <= (sizeof(arr)/sizeof(int)); i++) {
-		printf("%d ", arr[i]);
-	}
-	printf("\n");
-	
-	return 0;
+int main()
+{
+    int  arr[6] = {21 ,54 ,6 ,23, 45, 78};
+    int i;
+    quicksort(arr,0,5);
+    for(i=0;i<6;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
 }
 
-void quick_sort(int arr[]) {
-	int high = sizeof(arr)/sizeof(int);
-	
+void quicksort(int a[],int low,int high)
+{
+    int i = low;
+    int j = high;  
+    int temp = a[i]; 
+  
+    if( low > high)
+    {          
+       return ;
+    }
+    while(i < j) 
+        {
+            while((a[j] >= temp) && (i < j))
+            { 
+                j--; 
+            }
+            a[i] = a[j];
+            while((a[i] <= temp) && (i < j))
+            {
+                i++; 
+            }  
+            a[j]= a[i];
+        }
+     a[i] = temp;
+     quicksort(a, low, i-1);
+     quicksort(a, j+1, high);
 }
+ 
